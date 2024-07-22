00d06719e8dfc53aff5718e351b1cae877fa5b3f
Jay Wes <wesleyjay202@gmail.com>
2024-07-22 00:42:59 -0700

    Setup & Config

---
+++ movie_maniac/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <section className='app h-screen w-full bg-black text-white grid grid-rows-[80px_auto]'>
+      <nav className='navbar'>Navbar Content</nav>
+      <main>Main Content</main>
+    </section>
+  )
+}
+
+export default App
---
+++ movie_maniac/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: '#101010'
+      },
+      keyframes: {
+        press: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(0.95)' },
+        },
+      },
+      animation: {
+        press: 'press 0.2s ease-in-out',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      transform: ['active'],
+      scale: ['active'],
+    },
+  },
+  plugins: [],
+}
