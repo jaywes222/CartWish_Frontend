3dcf77e359ac0018f9b636b9f4c8056e3765cd72
Jay Wes <wesleyjay202@gmail.com>
2024-07-22 04:31:27 -0700

    Add MovieList Component

--- movie_maniac/src/App.jsx
+++ movie_maniac/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import MovieList from './components/MovieList/MovieList'
 
 const App = () => {
   return (
     <section className='app h-screen w-full bg-black text-white grid grid-rows-[80px_auto]'>
       <Navbar />
-      <main>Main Content</main>
+      <main>
+        <MovieList />
+      </main>
     </section>
   )
 }
--- movie_maniac/src/components/Navbar/Navbar.jsx
+++ movie_maniac/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import Party from '../../assets/partying-face.png'
 const Navbar = () => {
     return (
         <>
-            <nav className='navbar flex items-center justify-between py-0 px-32 border border-b-2 border-b-platinum'>
+            <nav className="navbar flex items-center justify-between py-0 px-32 border-b-2 border-b-platinum mb-0">
                 <h1 className='text-3xl text-canary'>Movie Maniac</h1>
 
                 <div className="navbar_links flex items-center text-white text-2xl font-medium space-x-8 py-4">
